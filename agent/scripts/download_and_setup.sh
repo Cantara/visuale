2aa96ec5e614a0df38994747e542aa61c3b29c6f
Thor Henning Hetland <totto@totto.org>
2020-01-27 14:44:06 +0100

    Update download_and_setup.sh

--- agent/scripts/download_and_setup.sh
+++ agent/scripts/download_and_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-wget
-wget
+wget https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/pushLocalhostHealthInfo.sh
+wget CRON
 
 crontab CRON
+
+echo "Installed push of health a a cron jobb.  See and configure pushLocalhostHealthInfo.sh to suit the installation"
