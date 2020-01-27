a00fe146753682818e8265062384d00fe8769d16
Thor Henning Hetland <totto@totto.org>
2020-01-27 14:58:06 +0100

    Update pushLocalhostHealthInfo.sh

--- agent/scripts/pushLocalhostHealthInfo.sh
+++ agent/scripts/pushLocalhostHealthInfo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 JSON="`wget -qO- http://localhost:8080/health`";
-wget -v --post-data "health=${JSON}" -X POST  https://visuale.cantara.no/devtst/myservice
+wget -v --post-data "health=${JSON}" -X POST  https://visuale.cantara.no/devtest/myservice
+
+# JSON2="`wget -qO- http://localhost:8080/info`";
+# wget -v --post-data "info=${JSON2}" -X POST  https://visuale.cantara.no/devtest/myservice
 
