9d29977d838069d04e68c9c6f39e5b37bd93d220
totto <totto@totto.org>
2020-03-24 23:21:26 +0100

    added more metadate to /health

--- src/main/java/no/cantara/tools/visuale/Main.java
+++ src/main/java/no/cantara/tools/visuale/Main.java
@@ -1,15 +1,32 @@
 package no.cantara.tools.visuale;
 
+import io.helidon.health.HealthSupport;
+import io.helidon.health.checks.HealthChecks;
 import io.helidon.microprofile.server.Server;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerConfiguration;
+import io.helidon.webserver.WebServer;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.logging.LogManager;
 
 /**
  * The application main class.
  */
 public final class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static String applicationInstanceName = "visuale";
 
     /**
      * Cannot be instantiated.
@@ -27,10 +44,33 @@ public final class Main {
         // load logging configuration
         setupLogging();
 
-        // start the server
-        Server server = startServer();
+        HealthSupport health = HealthSupport.builder()
+                .add(HealthChecks.healthChecks())
+                .add((HealthCheck) () -> HealthCheckResponse.named("exampleHealthCheck")
+                        .up()
+                        .withData("time", System.currentTimeMillis())
+                        .withData("version", getVersion())
+                        .build())
+                .build();
 
-        System.out.println("http://localhost:" + server.port() + "/status");
+        Routing routing = Routing.builder()
+                .register(health)
+                .get("/hello", (req, res) -> res.send("Hello World!"))
+                .build();
+
+        ServerConfiguration serverConfig = ServerConfiguration.builder()
+                .build();
+
+        WebServer ws = WebServer.create(serverConfig, routing);
+        // start the server
+        //Server server = startServer();
+        ws.start()
+                .thenApply(webServer -> {
+                    String endpoint = "http://localhost:" + webServer.port();
+                    System.out.println("Health checks available on: " + endpoint + "/health");
+                    System.out.println("Environment status available on:  " + endpoint + "/status");
+                    return null;
+                });
     }
 
     /**
@@ -52,5 +92,43 @@ public final class Main {
         try (InputStream is = Main.class.getResourceAsStream("/logging.properties")) {
             LogManager.getLogManager().readConfiguration(is);
         }
+    }
+
+
+    private static String getVersion() {
+        Properties mavenProperties = new Properties();
+        String resourcePath = "/META-INF/maven/no.cantara.tools/visuale/pom.properties";
+        URL mavenVersionResource = Main.class.getResource(resourcePath);
+        if (mavenVersionResource != null) {
+            try {
+                mavenProperties.load(mavenVersionResource.openStream());
+                return mavenProperties.getProperty("version", "missing version info in " + resourcePath) + " [" + applicationInstanceName + " - " + getMyIPAddresssesString() + "]";
+            } catch (IOException e) {
+                log.warn("Problem reading version resource from classpath: ", e);
+            }
+        }
+        return "(DEV VERSION)" + " [" + applicationInstanceName + " - " + getMyIPAddresssesString() + "]";
+    }
+
+    public static String getMyIPAddresssesString() {
+        String ipAdresses = "";
+
+        try {
+            ipAdresses = InetAddress.getLocalHost().getHostAddress();
+            Enumeration n = NetworkInterface.getNetworkInterfaces();
+
+            while (n.hasMoreElements()) {
+                NetworkInterface e = (NetworkInterface) n.nextElement();
+
+                InetAddress addr;
+                for (Enumeration a = e.getInetAddresses(); a.hasMoreElements(); ipAdresses = ipAdresses + "  " + addr.getHostAddress()) {
+                    addr = (InetAddress) a.nextElement();
+                }
+            }
+        } catch (Exception var5) {
+            ipAdresses = "Not resolved";
+        }
+
+        return ipAdresses;
     }
 }
