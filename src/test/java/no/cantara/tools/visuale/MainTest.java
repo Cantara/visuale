671bc228a296dc9dbe3ff1fa8115c624b1c48370
totto <totto@totto.org>
2020-03-24 21:05:14 +0100

    some testing on jenkins issue

--- src/test/java/no/cantara/tools/visuale/MainTest.java
+++ src/test/java/no/cantara/tools/visuale/MainTest.java
@@ -4,36 +4,50 @@ import io.helidon.microprofile.server.Server;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.spi.CDI;
-import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
 
-class MainTest {
+public class MainTest {
     private static Server server;
 
     @BeforeAll
     public static void startTheServer() throws Exception {
         server = Main.startServer();
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
     }
 
-//    @Test()  // for some reason this fails in jenkins as of now
-    void testMockedStatus() {
+    @Test()  // for some reason this fails in jenkins as of now
+    public void testMockedStatus() {
 
         Client client = ClientBuilder.newClient();
 
-        JsonObject jsonObject = client
-                .target(getConnectionString("/status"))
+//        JsonObject jsonObject = client
+//                .target(getConnectionString("/status"))
+//                .request()
+//                .get(JsonObject.class);
+//        Assertions.assertTrue(jsonObject.getString("message").length() > 20);
+
+
+        Response r = client
+                .target(getConnectionString("/metrics"))
                 .request()
-                .get(JsonObject.class);
-        Assertions.assertTrue(jsonObject.getString("message").length() > 20);
+                .get();
+        Assertions.assertEquals(200, r.getStatus(), "GET metrics status code");
+
+        r = client
+                .target(getConnectionString("/health"))
+                .request()
+                .get();
+        Assertions.assertEquals(200, r.getStatus(), "GET health status code");
     }
 
     @AfterAll
-    static void destroyClass() {
+    public static void destroyClass() {
         CDI<Object> current = CDI.current();
         ((SeContainer) current).close();
     }
