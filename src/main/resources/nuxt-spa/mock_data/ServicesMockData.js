eb98554a67babc20e2137a718dcacc62370e8a93
Marius Årnes <marius.appa@gmail.com>
2020-03-25 21:16:43 +0100

    added Mockdata, Vuex store, global scss variables

---
+++ src/main/resources/nuxt-spa/mock_data/ServicesMockData.js
@@ -0,0 +1,56 @@
+export function ServicesMockData (){
+  return {
+    "name": "Quadim-QA",
+    "services": [
+      {
+        "name": "Overlord-Service",
+        "nodes": [
+          {
+            "ip": "10.45.54.23",
+            "health": [
+              {
+                "Status": "true",
+                "now": "2020-03-24T18:34:35.987Z",
+                "running since": "2020-03-23T09:11:49.070Z",
+                "version": "0.61.34"
+              }
+            ]
+          },
+          {
+            "ip": "10.45.54.24",
+            "health": [
+              {
+                "Status": "true",
+                "now": "2020-03-24T18:34:35.987Z",
+                "running since": "2020-03-23T09:11:49.070Z",
+                "version": "0.61.34"
+              }
+            ]
+          },
+          {
+            "ip": "10.45.54.25",
+            "health": [
+              {
+                "Status": "true",
+                "now": "2020-03-24T18:34:35.987Z",
+                "running since": "2020-03-23T09:11:49.070Z",
+                "version": "0.61.34"
+              }
+            ]
+          },
+          {
+            "ip": "10.45.54.26",
+            "health": [
+              {
+                "Status": "true",
+                "now": "2020-03-24T18:34:35.987Z",
+                "running since": "2020-03-23T09:11:49.070Z",
+                "version": "0.61.34"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
---
+++ src/main/resources/nuxt-spa/store/index.js
@@ -0,0 +1,5 @@
+import {ServicesMockData} from "../mock_data/ServicesMockData";
+
+export const state = () => ({
+  services: ServicesMockData(),
+});
