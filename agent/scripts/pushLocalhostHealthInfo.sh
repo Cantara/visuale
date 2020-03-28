09804d5ad39426baf66a8b42004a0cb82a2acb25
totto <totto@totto.org>
2020-03-28 20:37:16 +0100

    probably got a bit further..

--- agent/scripts/pushLocalhostHealthInfo.sh
+++ agent/scripts/pushLocalhostHealthInfo.sh
@@ -6,5 +6,5 @@ wget --method=PUT --body-data="${JSON}"  https://visuale.cantara.no/status
 # JSON2="`wget -qO- http://localhost:8080/info`";
 # wget --method=PUT --body-data="${JSON}"  http://localhost:35277/status
 
-# JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"  http://localhost:35277/status
+# JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status
 
