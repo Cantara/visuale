5cf97729f1751de44055de7a6187e88093a63993
Marius Årnes <marius.appa@gmail.com>
2020-03-31 12:06:55 +0200

    service names are not sorted alphabetically

--- src/main/resources/nuxt-spa/store/index.js
+++ src/main/resources/nuxt-spa/store/index.js
@@ -1,10 +1,20 @@
 
+function sortServicesByName(payload) {
+  payload.sort(function(a, b) {
+    const serviceA = a.name.toUpperCase();
+    const serviceB = b.name.toUpperCase();
+    return (serviceA < serviceB) ? -1 : (serviceA > serviceB) ? 1 : 0;
+  });
+
+}
+
 export const state = () => ({
   services: {},
 
 });
 export const mutations = {
   setData (state, payload) {
+   sortServicesByName(payload.services);
     state.services = payload;
   }
 };
