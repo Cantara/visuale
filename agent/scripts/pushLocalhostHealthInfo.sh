dfe4216fdb07e235e1df6fb6f4c5f4408922244e
totto <totto@totto.org>
2020-04-01 13:45:54 +0200

    minor

--- agent/scripts/pushLocalhostHealthInfo.sh
+++ agent/scripts/pushLocalhostHealthInfo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-JSON="`wget -qO- https://whydahdev.cantara.no/sso/health`";
-wget --method=PUT --body-data="${JSON}"  https://visuale.cantara.no/status
+JSON="`wget -qO- https://whydahdev.cantara.no/sso/health`";wget --method=PUT --body-data="${JSON}"  https://visuale.cantara.no/status
 
 # JSON2="`wget -qO- http://localhost:8080/info`";
 # wget --method=PUT --body-data="${JSON}"  http://localhost:35277/status
