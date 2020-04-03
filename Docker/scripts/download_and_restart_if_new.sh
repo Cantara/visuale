3b0b8578546e8831492df9a3ea1501fc74917b32
totto <totto@totto.org>
2020-04-03 14:50:54 +0200

    added missing script

---
+++ Docker/scripts/download_and_restart_if_new.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+JSON="`wget -qO-  http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status
+
+./scripts/semantic_update_service.sh
+
+if [ $? -eq 0 ]
+then
+  ./scripts/kill-service.sh
+  ./scripts/start-service.sh
+  echo "Successfully updated service"
+else
+  echo "No updates found, doing nothing"
+fi
+
+
