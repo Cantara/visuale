67a3e51dc737689bc7bc7c0d7ddfc0fdad631c82
Marius Årnes <marius.appa@gmail.com>
2020-04-03 22:45:32 +0200

    Node Refactoring

--- src/main/resources/nuxt-spa/components/service/Node.vue
+++ src/main/resources/nuxt-spa/components/service/Node.vue
@@ -1,26 +1,25 @@
 <template>
-  <div @click="showModal = true" class="node-block" :style="flexStyle">
-    <div class="marker">
-      <div>
-        <div class="title">
-        <NodeTrafficLight :node="node"/> <span>{{title | truncateText(15)}}</span>
-        </div>
-        <div class="content">
-          <ul>
-            <li><span>Healthy: </span>{{node.is_healthy}}</li>
-            <li><span>Ver: </span>{{(node.health[0].hasOwnProperty('version') ? node.health[0].version : 'missing')| truncateText(13)}}</li>
-            <li><NodeUptime :health="node.health[0]" /></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <Modal  v-if="showModal" :title="node.ip" @close="showModal = false">
-      <JsonModal :text="node"></JsonModal>
-    </Modal>
-  </div>
+  <NodeElement @click="showModal = true">
+    <template v-slot:title>
+      <NodeTrafficLight :node="node"/> <span>{{title | truncateText(15)}}</span>
+    </template>
+    <template v-slot:content>
+      <ul>
+        <li><span>Healthy: </span>{{node.is_healthy}}</li>
+        <li><span>Ver: </span>{{(node.health[0].hasOwnProperty('version') ? node.health[0].version : 'missing')| truncateText(13)}}</li>
+        <li><NodeUptime :health="node.health[0]" /></li>
+      </ul>
+    </template>
+    <template v-slot:modal>
+      <Modal  v-if="showModal" :title="node.ip" @close="showModal = false">
+        <JsonModal :text="node"></JsonModal>
+      </Modal>
+    </template>
+  </NodeElement>
 </template>
 
 <script>
+  import NodeElement from "./node/NodeElement";
   import NodeTrafficLight from "./node/NodeTrafficLight";
   import NodeUptime from "./node/NodeUptime";
   import Modal from "../Modal";
@@ -31,7 +30,8 @@
       NodeTrafficLight,
       NodeUptime,
       Modal,
-      JsonModal
+      JsonModal,
+      NodeElement
     },
     data() {
       return {
@@ -45,76 +45,13 @@
         if(this.node.hasOwnProperty('name'))
           return this.node.name;
         return 'Missing'
-
         }
     },
     props: {
       node: {
         required: true,
         type: Object
-      },
-      flexStyle: {
-        required: false,
-        type: Object
       }
     }
   }
 </script>
-
-<style lang="scss" scoped>
-  @import '~/assets/styles/variables/_variables.scss';
-
-
-
-  .node-block {
-    border: 1px solid white;
-    border-radius: 10px;
-  }
-
-  .marker > div {
-    padding: 0;
-    margin-top: -0.55em;
-    text-align: center;
-  }
-
-  .title {
-    background: $color--background none repeat scroll 0 0;
-    display: flex;
-    padding: 0 0.6em;
-    margin: 0 0.6em;
-    color: white;
-    line-height: 1rem;
-    float: left;
-    font-size: 0.9rem;
-  }
-  .title >span {
-    padding: 0 0 0 0.35em;
-  }
-
-  .content {
-    padding: 0;
-    color: white;
-  }
-  ul{
-    padding: 0 0.2em 0.3em 0.3em;
-    list-style:none;
-    text-align:left;
-    font-size:0.9rem;
-  }
-  li {
-    color:white;
-    font-weight: 700;
-  }
-  span{
-    color: $color--description!important;
-    font-weight:400;
-  }
-  td {
-    color: $color--description;
-    text-align: right;
-  }
-
-  th {
-    text-align: left;
-  }
-</style>
---
+++ src/main/resources/nuxt-spa/components/service/node/NodeElement.vue
@@ -0,0 +1,89 @@
+<template>
+  <div @click="$emit('click')" class="node-block">
+    <div class="marker">
+      <div>
+        <div>
+          <div class="title">
+            <slot name="title">
+
+            </slot>
+          </div>
+        </div>
+        <div class="content">
+          <slot name="content">
+
+          </slot>
+        </div>
+      </div>
+    </div>
+    <slot name="modal">
+
+    </slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "NodeElement"
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import '~/assets/styles/variables/_variables.scss';
+
+
+
+  .node-block {
+    border: 1px solid white;
+    border-radius: 10px;
+  }
+
+  .marker > div {
+    padding: 0;
+    margin-top: -0.55em;
+    text-align: center;
+    display:flex;
+    flex-direction: column;
+  }
+
+  .title {
+    background: $color--background none repeat scroll 0 0;
+    display: flex;
+    padding: 0 0.6em;
+    margin: 0 0.6em;
+    color: white;
+    line-height: 1rem;
+    float: left;
+    font-size: 0.9rem;
+  }
+  .title >span {
+    padding: 0 0 0 0.35em;
+  }
+
+  .content {
+    padding: 0;
+    color: white;
+  }
+  ul{
+    padding: 0 0.2em 0.3em 0.3em;
+    list-style:none;
+    text-align:left;
+    font-size:0.9rem;
+  }
+  li {
+    color:white;
+    font-weight: 700;
+  }
+  span{
+    color: $color--description!important;
+    font-weight:400;
+  }
+  td {
+    color: $color--description;
+    text-align: right;
+  }
+
+  th {
+    text-align: left;
+  }
+</style>
