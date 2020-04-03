3dc7c5e877fb39473a8536522fe0a6e6555acb13
totto <totto@totto.org>
2020-04-03 18:16:00 +0200

    clean up in agent example script

--- agent/scripts/pushLocalhostHealthInfo.sh
+++ agent/scripts/pushLocalhostHealthInfo.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-JSON="`wget -qO- https://whydahdev.cantara.no/sso/health`";wget --method=PUT --body-data="${JSON}"  https://visuale.cantara.no/status
+JSON="`wget -qO- http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"  http://localhost:8080/status
+JSON="`wget -qO- http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"  http://localhost:8080/environment/visuale/myinstance
 
-# JSON2="`wget -qO- http://localhost:8080/info`";
-# wget --method=PUT --body-data="${JSON}"  http://localhost:35277/status
-
+# Other examples
+#
+#
+# JSON="`wget -qO- http://localhost:8080/info`"; wget --method=PUT --body-data="${JSON}"  http://localhost:35277/status
+#
 # JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status
 #
 # JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   http://localhost:8080/status/test/myservice/mynode
+# JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status/test/myservice/mynode
+#
+# JSON=$(<example2_JsonHealth.json ) ;wget --method=PUT --body-data="${JSON}"  http://localhost:8080/health
+
 
