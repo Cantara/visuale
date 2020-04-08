23e0b48548af38dba63022d6cb7b46d5f93a401e
totto <totto@totto.org>
2020-04-08 07:29:40 +0200

    enhanced and clean up the Dockerfile

--- Docker/runDocker.sh
+++ Docker/runDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo docker run --it -p 8080:8080 visuale:latest
+sudo docker run --rm -p 8080:8080 visuale:latest
 
 echo "Now try wget://http://localhost:8080/ to see the Visuale Dashboard"
