815535134c87e8364cb1424db816af5235dc5426
totto <totto@totto.org>
2020-06-04 22:08:26 +0200

    minor

--- agent/scripts/pushLocalhostHealthInfo.sh
+++ agent/scripts/pushLocalhostHealthInfo.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-JSON="`wget -qO- http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"  http://localhost:8080/status
-JSON="`wget -qO- http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"  http://localhost:8080/status/environment/visuale/myinstance
-
+for n in 1 2 3 4 5 6 7 8 9
+do
+  JSON="`wget -qO- http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"  http://localhost:8080/status
+  JSON="`wget -qO- http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"  http://localhost:8080/status/environment/visuale/myinstance
+done
 # Other examples
 #
 #
-# JSON="`wget -qO- http://localhost:8080/info`"; wget --method=PUT --body-data="${JSON}"  http://localhost:35277/status
+# JSON="`wget -qO- http://localhost:8080/info`"; wget --method=PUT --body-data="${JSON}"  http://localhost:35277/api/status
 #
-# JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status
+# JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/api/status
 #
 # JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   http://localhost:8080/status/test/myservice/mynode
 # JSON=$(<exampleJsonHealth.json ); wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status/test/myservice/mynode
