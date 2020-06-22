120b4a089e600fa4b14a1a968acb7a7678fe5921
Marius Årnes <marius.appa@gmail.com>
2020-06-22 22:40:03 +0200

    auto refresh page every 30 minutes

--- src/main/resources/nuxt-spa/nuxt.config.js
+++ src/main/resources/nuxt-spa/nuxt.config.js
@@ -10,7 +10,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {'http-equiv': "refresh", content:"1800"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
