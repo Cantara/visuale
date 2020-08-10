9a30af4d2d148f8da7192c8407f9f40c4f3184e0
totto <totto@totto.org>
2020-08-10 11:59:03 +0200

    added a more complete shell script for detecting and reporting service status from commandline/cron

--- agent/scripts/reportServiceHealthToVisuale_CronScript.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 source ./reportServiceHealthToVisuale_CronScript.properties
-for n in 1 2 3 4 5 6 7 8 9 10
-do
-  if JSON="`wget -qO-  $healthUrl`";wget --method=PUT --body-data="${JSON}" $reportToUrl1; then
-    JSON="`wget -qO-  $healthUrl`";wget --method=PUT --body-data="${JSON}" $reportToUrl2
+for n in 1 2 3 4 5 6 7 8 9 10; do
+  if
+    JSON="$(wget -qO- $healthUrl)"
+    wget --method=PUT --body-data="${JSON}" $reportToUrl1
+  then
+    JSON="$(wget -qO- $healthUrl)"
+    wget --method=PUT --body-data="${JSON}" $reportToUrl2
   else
-    JSON=$(<Visuale_FAIL.json ); wget --method=PUT --body-data="${JSON}" $reportToUrl1
-    JSON=$(<Visuale_FAIL.json ); wget --method=PUT --body-data="${JSON}" $reportToUrl2
+    JSON=$(<Visuale_FAIL.json)
+    wget --method=PUT --body-data="${JSON}" $reportToUrl1
+    JSON=$(<Visuale_FAIL.json)
+    wget --method=PUT --body-data="${JSON}" $reportToUrl2
   fi
   sleep 4
 done
