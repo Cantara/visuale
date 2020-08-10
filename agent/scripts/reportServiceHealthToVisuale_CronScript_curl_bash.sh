0cec6d537f19cfbaacbab67eb559e7086c0a627a
totto <totto@totto.org>
2020-08-10 17:43:21 +0200

    added curl and bash version of script for such environments

--- agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
@@ -10,13 +10,13 @@ for n in 1 2 3 4 5 6 7 8 9 10; do
     JSON="$(wget -qO- $healthUrl)"
     curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d ${JSON} | sed -e 's/^"//' -e 's/"$//'
 
-#    wget --method=PUT --body-data="${JSON}" $reportToUrl2
+    #    wget --method=PUT --body-data="${JSON}" $reportToUrl2
   else
     JSON=$(<./scripts/ConfigService_FAIL.json)
     curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d ${JSON} | sed -e 's/^"//' -e 's/"$//'
     JSON=$(<./scripts/ConfigService_FAIL.json)
     curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d ${JSON} | sed -e 's/^"//' -e 's/"$//'
-#    wget --method=PUT --body-data="${JSON}" $reportToUrl2
+    #    wget --method=PUT --body-data="${JSON}" $reportToUrl2
   fi
   sleep 4
 done
