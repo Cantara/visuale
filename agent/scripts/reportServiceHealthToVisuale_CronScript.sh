dc251477a5f3007e6e24d8d0082abf7b2da7d73f
totto <totto@totto.org>
2020-08-10 11:36:02 +0200

    added a more complete shell script for detecting and reporting service status from commandline/cron

--- agent/scripts/reportServiceHealthToVisuale_CronScript.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-for n in 1 2 3 4 5 6 7 8 9 10
-do
-  if JSON="`wget -qO-  http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}" https://visuale-devtest.cantara.no/api/status/devtest/Visuale%20DEVTEST%20Dash
-board/node?service_tag=SoftwareFactory; then
-    JSON="`wget -qO-  http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}" https://visuale.cantara.no/api/status/prod/isuale%20DEVTEST%20Dashboard/nod
-e?service_tag=SoftwareFactory
+for n in 1 2 3 4 5 6 7 8 9 10; do
+  if
+    JSON="$(wget -qO- http://localhost:8080/health)"
+    wget --method=PUT --body-data="${JSON}" https://visuale-devtest.cantara.no/api/status/devtest/Visuale%20DEVTEST%20Dash
+    board/node?service_tag=SoftwareFactory
+  then
+    JSON="$(wget -qO- http://localhost:8080/health)"
+    wget --method=PUT --body-data="${JSON}" https://visuale.cantara.no/api/status/prod/isuale%20DEVTEST%20Dashboard/nod
+    e?service_tag=SoftwareFactory
   else
-    JSON=$(<Visuale_FAIL.json ); wget --method=PUT --body-data="${JSON}" https://visuale-devtest.cantara.no/api/status/devtest/Visuale%20DEVTEST%20Dashboard/node?service_t
-ag=SoftwareFactory
-    JSON=$(<Visuale_FAIL.json ); wget --method=PUT --body-data="${JSON}" https://visuale.cantara.no/api/status/prod/Visuale%20DEVTEST%20Dashboard/node?service_tag=Softw
-areFactory
+    JSON=$(<Visuale_FAIL.json)
+    wget --method=PUT --body-data="${JSON}" https://visuale-devtest.cantara.no/api/status/devtest/Visuale%20DEVTEST%20Dashboard/node?service_t
+    ag=SoftwareFactory
+    JSON=$(<Visuale_FAIL.json)
+    wget --method=PUT --body-data="${JSON}" https://visuale.cantara.no/api/status/prod/Visuale%20DEVTEST%20Dashboard/node?service_tag=Softw
+    areFactory
   fi
   sleep 4
 done
