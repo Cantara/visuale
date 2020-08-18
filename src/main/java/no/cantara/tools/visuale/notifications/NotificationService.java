4ca9c926cc429f957e30735b9c4f82f403a80f6c
totto <totto@totto.org>
2020-08-18 20:48:19 +0200

    Added a start to support sending warning and alarm notifications in NotificationService

--- src/main/java/no/cantara/tools/visuale/notifications/NotificationService.java
+++ src/main/java/no/cantara/tools/visuale/notifications/NotificationService.java
@@ -1,10 +1,15 @@
 package no.cantara.tools.visuale.notifications;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,27 +17,47 @@ import java.util.concurrent.ConcurrentHashMap;
 public class NotificationService {
 
     public static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+    public static ObjectMapper mapper = new ObjectMapper().configure(com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
 
     private static final String alarmFilename = "./logs/service-notification-alarms.log";
     private static final String warningFilename = "./logs/service-notification-warnings.log";
+    private static final String notificationStateFilename = "./logs/service-notification-mapname.json";
 
     public static Map warningMap = new ConcurrentHashMap();
     public static Map alarmMap = new ConcurrentHashMap<>();
     private static boolean initialBootWarning = true;
     private static boolean initialBootAlarm = true;
 
+    private static boolean loadedStateFromFile = false;
+
+//    static {
+//        sendAlarm("test", "atull");
+//        sendWarning("test", "wtull");
+//        clearService("test");
+//    }
+
     public static boolean sendWarning(String service, String warningMessage) {
+        if (!loadedStateFromFile) {
+            restoreNotificationStateMaps();
+            loadedStateFromFile = true;
+        }
         if (warningMap.get(service) == null) {
             warningMap.put(service, warningMessage);
             appendWarningToFile(service, warningMessage, false);
+            storeNotificationStateMaps();
         }
         return true;
     }
 
     public static boolean sendAlarm(String service, String alarmMessage) {
+        if (!loadedStateFromFile) {
+            restoreNotificationStateMaps();
+            loadedStateFromFile = true;
+        }
         if (alarmMap.get(service) == null) {
             alarmMap.put(service, alarmMessage);
             appendAlarmToFile(service, alarmMessage, false);
+            storeNotificationStateMaps();
         }
         return true;
     }
@@ -40,19 +65,19 @@ public class NotificationService {
     public static boolean clearService(String service) {
         if (alarmMap.get(service) != null) {
             alarmMap.remove(service);
-            appendAlarmToFile(service, "", false);
+            appendAlarmToFile(service, "", true);
         }
         if (warningMap.get(service) != null) {
             warningMap.remove(service);
-            appendWarningToFile(service, "", false);
+            appendWarningToFile(service, "", true);
         }
         return true;
     }
 
-    private static void appendAlarmToFile(String service, String amessage, boolean cleared) {
+    private static synchronized void appendAlarmToFile(String service, String amessage, boolean cleared) {
         try {
             FileWriter fileWriter;
-            if (initialBootAlarm) {
+            if (initialBootAlarm && !cleared) {
                 initialBootAlarm = false;
                 fileWriter = new FileWriter(alarmFilename, false);
             } else {
@@ -61,23 +86,20 @@ public class NotificationService {
             PrintWriter printWriter = new PrintWriter(fileWriter, true);
             if (!cleared) {
                 printWriter.println("Alarm for " + service);
-                printWriter.println("    " + Instant.now().toString() + " - " + amessage + " - \n");
+                printWriter.println("    " + Instant.now().toString() + " - " + amessage);
             } else {
-                printWriter.println("Alarm for " + service + " cleared - time: " + Instant.now().toString());
+                printWriter.println("Cleared Alarm for " + service + " - time: " + Instant.now().toString());
             }
             printWriter.flush();
-            printWriter.close();
-            fileWriter.flush();
-            fileWriter.close();
         } catch (Exception e) {
-            logger.error("Unable to send and persist alarm");
+            logger.error("Unable to send and persist alarm", e);
         }
     }
 
-    private static void appendWarningToFile(String service, String mwessage, boolean cleared) {
+    private static synchronized void appendWarningToFile(String service, String mwessage, boolean cleared) {
         try {
             FileWriter fileWriter;
-            if (initialBootWarning) {
+            if (initialBootWarning && !cleared) {
                 initialBootWarning = false;
                 fileWriter = new FileWriter(warningFilename, false);
             } else {
@@ -86,16 +108,75 @@ public class NotificationService {
             PrintWriter printWriter = new PrintWriter(fileWriter, true);
             if (!cleared) {
                 printWriter.println("Warning for " + service);
-                printWriter.println("    " + Instant.now().toString() + " - " + mwessage + "\n");
+                printWriter.println("    " + Instant.now().toString() + " - " + mwessage);
             } else {
-                printWriter.println("Warning for " + service + " cleared - time: " + Instant.now().toString());
+                printWriter.println("Cleared Warning for " + service + "  - time: " + Instant.now().toString());
             }
-            printWriter.flush();
+
             printWriter.close();
-            fileWriter.flush();
-            fileWriter.close();
         } catch (Exception e) {
-            logger.error("Unable to send and persist warning");
+            logger.error("Unable to send and persist warning", e);
         }
+    }
+
+    private synchronized static void storeNotificationStateMaps() {
+        if (warningMap.size() > 0) {
+            String warningMapFilename = notificationStateFilename.replace("mapname", "warning");
+            try {
+                FileWriter fileWriter = new FileWriter(warningMapFilename, false);
+                PrintWriter printWriter = new PrintWriter(fileWriter, true);
+                printWriter.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(warningMap));
+
+                printWriter.close();
+            } catch (Exception e) {
+                logger.error("Unable to send and persist warning state", e);
+            }
+        }
+        if (alarmMap.size() > 0) {
+            String alarmMapFilename = notificationStateFilename.replace("mapname", "alarm");
+            try {
+                FileWriter fileWriter = new FileWriter(alarmMapFilename, false);
+                PrintWriter printWriter = new PrintWriter(fileWriter, true);
+                printWriter.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(alarmMap));
+                printWriter.close();
+            } catch (Exception e) {
+                logger.error("Unable to send and persist alarm state", e);
+            }
+        }
+    }
+
+    private static boolean restoreNotificationStateMaps() {
+        if (loadedStateFromFile) {
+            return true;
+        }
+        loadedStateFromFile = true;
+        String warningMapFileName = notificationStateFilename.replace("mapname", "warning");
+        try {
+            File file = new File(warningMapFileName);
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+
+            String content = new String(Files.readAllBytes(Paths.get(warningMapFileName)), StandardCharsets.UTF_8);
+            if (content != null && content.length() > 4) {
+                warningMap = mapper.readValue(content, Map.class);
+            }
+        } catch (Exception e) {
+            logger.error("Unable to restore warning state", e);
+
+        }
+        String alarmMapFileName = notificationStateFilename.replace("mapname", "alarm");
+        try {
+            File file = new File(alarmMapFileName);
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+            String content = new String(Files.readAllBytes(Paths.get(alarmMapFileName)), StandardCharsets.UTF_8);
+            if (content != null && content.length() > 4) {
+                alarmMap = mapper.readValue(content, Map.class);
+            }
+        } catch (Exception e) {
+            logger.error("Unable to restore alarm state", e);
+
+        }
+        return true;
     }
 }
