1d76472fc2cd3a5dfec513da9ecc63388fe6f556
totto <totto@totto.org>
2020-10-24 11:02:29 +0200

    Added support for restart if no running process found even if there are no updates

--- scripts/download_and_restart_if_new.sh
+++ scripts/download_and_restart_if_new.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-JSON="`wget -qO-  http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status/test/visuale/demo
+
+# JSON="`wget -qO-  http://localhost:8080/health`";wget --method=PUT --body-data="${JSON}"   https://visuale.cantara.no/status/test/visuale/demo
 
 ./scripts/semantic_update_service.sh
 
@@ -9,5 +10,12 @@ then
   ./scripts/start-service.sh
   echo "Successfully updated service"
 else
-  echo "No updates found, doing nothing"
+  echo "No updates found"
+  if ps -ef | grep java | grep visuale.jar; then
+     echo "Running process found - doing nothing"
+  else
+     echo "Running process not found - restarting process"
+    ./scripts/start-service.sh
+  fi
+
 fi
