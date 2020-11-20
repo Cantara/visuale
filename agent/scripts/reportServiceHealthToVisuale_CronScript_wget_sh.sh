968786a184f417429d1f7e2746c4b49f4e90690d
totto <totto@totto.org>
2020-11-20 14:11:54 +0100

    Optimizing script to support large json payloads

--- agent/scripts/reportServiceHealthToVisuale_CronScript_wget_sh.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript_wget_sh.sh
@@ -7,12 +7,12 @@ for n in 1 2 3 4 5 6 7 8 9 10; do
   then
     echo "UP"
 #    echo $JSON
-#  Supporting larhe json loads
-    echo $JSON | wget --method=PUT --body-data=@- $reportToUrl1
-    echo $JSON | wget --method=PUT --body-data=@- $reportToUrl2
+#  Supporting larhe json loads - not working
+#    echo $JSON | wget --method=PUT --body-data=@- $reportToUrl1
+#    echo $JSON | wget --method=PUT --body-data=@- $reportToUrl2
 #  Old version, will break with too many argumnets if json is too big
-#    wget --method=PUT --body-data="$JSON" $reportToUrl1
-#    wget --method=PUT --body-data="$JSON" $reportToUrl2
+    wget --method=PUT --body-data="$JSON" $reportToUrl1
+    wget --method=PUT --body-data="$JSON" $reportToUrl2
   else
     echo "down"
     JSON=$(<./scripts/Visuale_FAIL.json)
