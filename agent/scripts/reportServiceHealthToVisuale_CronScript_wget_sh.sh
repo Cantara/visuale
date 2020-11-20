974d57350b801df364e3f0e683a350b74fae2c17
totto <totto@totto.org>
2020-11-20 11:59:55 +0100

    fixed script broken by shell-formatter in IDEA

--- agent/scripts/reportServiceHealthToVisuale_CronScript_wget_sh.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript_wget_sh.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 source ./scripts/reportServiceHealthToVisuale_CronScript.properties
+
 for n in 1 2 3 4 5 6 7 8 9 10; do
-  if
-    JSON="$(wget -qO- $healthUrl)"
-    wget --method=PUT --body-data="${JSON}" $reportToUrl1
+  if JSON="$(wget -qO- $healthUrl)" ;
   then
-    JSON="$(wget -qO- $healthUrl)"
-    wget --method=PUT --body-data="${JSON}" $reportToUrl2
+    echo "UP"
+#    echo $JSON
+    wget --method=PUT --body-data="$JSON" $reportToUrl1
+    wget --method=PUT --body-data="$JSON" $reportToUrl2
   else
+    echo "down"
     JSON=$(<Visuale_FAIL.json)
     wget --method=PUT --body-data="${JSON}" $reportToUrl1
-    JSON=$(<Visuale_FAIL.json)
     wget --method=PUT --body-data="${JSON}" $reportToUrl2
   fi
   sleep 4
 done
+
