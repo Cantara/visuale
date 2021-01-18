09035a157f03b214503b47ae680c0f654cf9aad0
Thor Henning Hetland <totto@totto.org>
2021-01-18 20:02:17 +0100

    Update reportServiceHealthToVisuale_CronScript_curl_bash.sh

--- agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
@@ -3,18 +3,16 @@
 source ./scripts/reportServiceHealthToVisuale.properties
 
 for n in 1 2 3 4 5 6 7 8 9 10; do
-  if JSON="$(wget -qO- $healthUrl)" ;
+  if JSON=$(curl   $healthUrl) ;
   then
     echo "UP"
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d ${JSON} | sed -e 's/^"//' -e 's/"$//'
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d ${JSON} | sed -e 's/^"//' -e 's/"$//'
-    #    wget --method=PUT --body-data="${JSON}" $reportToUrl2
+    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
+    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
   else
     echo "Down"
     JSON=$(<./scripts/Visuale_FAIL.json)
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d ${JSON} | sed -e 's/^"//' -e 's/"$//'
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d ${JSON} | sed -e 's/^"//' -e 's/"$//'
-    #    wget --method=PUT --body-data="${JSON}" $reportToUrl2
+    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
+    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
   fi
   sleep 4
 done
