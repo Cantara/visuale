5c059bdc71c5f640276725308d16545eafd165ce
Thor Henning Hetland <totto@totto.org>
2021-05-03 14:51:11 +0200

    added service scripts to installation

--- agent/scripts/download_and_setup_visuale_reporting.sh
+++ agent/scripts/download_and_setup_visuale_reporting.sh
@@ -7,14 +7,23 @@ cd .
 mkdir scripts
 cd scripts
 
-wget -O reportServiceHealthToVisuale.properties https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/reportServiceHealthToVisuale.properties
-wget https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/Visuale_SERVICE_FAIL.json
+wget -O reportServiceHealthToVisuale.properties_template https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/reportServiceHealthToVisuale.properties
+wget -O Visuale_SERVICE_FAIL.json_template https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/Visuale_SERVICE_FAIL.json
 wget https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
 wget https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/reportServiceHealthToVisuale_CronScript_wget_sh.sh
 ln -s reportServiceHealthToVisuale_CronScript_wget_sh.sh reportServiceHealthToVisuale.sh
+
+
+wget https://raw.githubusercontent.com/Cantara/visuale/master/scripts/download_and_restart_if_new.sh
+wget https://raw.githubusercontent.com/Cantara/visuale/master/scripts/semantic_update_service.sh
+wget -O semantic_update_service.properties_template https://github.com/Cantara/visuale/blob/master/scripts/semantic_update_service.properties
+
+wget -O kill-service.sh_template https://raw.githubusercontent.com/Cantara/visuale/master/scripts/kill-service.sh
+wget -O start-service.sh https://raw.githubusercontent.com/Cantara/visuale/master/scripts/start-service.sh
+
 chmod 755 *.sh
 
-wget https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/CRON
+wget -O CRON_template https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/CRON
 
 cd .
 # ln -s ./scripts/CRON CRON
