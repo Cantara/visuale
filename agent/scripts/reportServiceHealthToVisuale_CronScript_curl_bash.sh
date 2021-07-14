e27906402968a9f8344d84e88a3a13768b11c0b2
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-07-14 11:11:53 +0200

    Use BASH_SOURCE and silent curl

--- agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
@@ -3,10 +3,10 @@
 #     wget https://raw.githubusercontent.com/Cantara/visuale/master/agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
 #
 
-source ./scripts/reportServiceHealthToVisuale.properties
+source $(dirname $BASH_SOURCE)/reportServiceHealthToVisuale.properties
 
 for n in 1 2 3 4 5 6 7 8 9 10; do
-  if JSON=$(curl   $healthUrl) ;
+  if JSON=$(curl --silent $healthUrl) ;
   then
     echo "UP"
     curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
