8cd36ce5862407e14b6cc2b07f47f7bc9112aaf9
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-07-14 15:14:22 +0200

    Reliable json PUT with curl

--- agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
+++ agent/scripts/reportServiceHealthToVisuale_CronScript_curl_bash.sh
@@ -6,16 +6,14 @@
 source $(dirname $BASH_SOURCE)/reportServiceHealthToVisuale.properties
 
 for n in 1 2 3 4 5 6 7 8 9 10; do
-  if JSON=$(curl --silent $healthUrl) ;
+  if json=$(curl --silent $healthUrl) ;
   then
     echo "UP"
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
   else
     echo "Down"
-    JSON=$(<./scripts/Visuale_SERVICE_FAIL.json)
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl1}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
-    curl --silent -X PUT -H "Content-Type: application/json" "${reportToUrl2}" -d '${JSON}' | sed -e 's/^"//' -e 's/"$//'
+    json=$(<./scripts/Visuale_SERVICE_FAIL.json)
   fi
+  printf "%s" "$json" | curl --silent -X PUT -H "Content-Type: application/json" -X PUT --data-binary @- "$reportToUrl1"
+  printf "%s" "$json" | curl --silent -X PUT -H "Content-Type: application/json" -X PUT --data-binary @- "$reportToUrl2"
   sleep 4
 done
