7508c4d0df31be7bd87e312a7592760cc7e606a3
black <marius.appa@gmail.com>
2021-08-02 19:43:51 +0200

    replace vue-fragment with vue-frag, register custom Fragment.vue component globally so we don't need to rename <fragment> components, updated node-sass to version 6.0.0 which is dependant on nodejs version 16, so updated pom.xml frontend-maven-plugin to utilize version 16.

---
+++ src/main/resources/nuxt-spa/components/Fragment.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-frag>
+    <slot />
+  </div>
+</template>
+
+<script>
+import frag from 'vue-frag';
+
+export default {
+  directives: {
+    frag
+  }
+};
+</script>
--- src/main/resources/nuxt-spa/plugins/plugins.js
+++ src/main/resources/nuxt-spa/plugins/plugins.js
@@ -1,9 +1,16 @@
 import  Vue  from  "vue";
 
 import vClickOutside from "v-click-outside"
-import Fragment from 'vue-fragment'
 
 Vue.use(vClickOutside);
 Vue.config.performance = process.env.NODE_ENV !== 'production'
 
-Vue.use(Fragment.Plugin)
+
+import frag from 'vue-frag';
+Vue.directive('fragment', frag);
+
+import Fragment from "~/components/Fragment";
+const components = { Fragment }
+Object.entries(components).forEach(([name, component]) => {
+  Vue.component(name, component)
+})
