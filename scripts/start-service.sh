9a9145081b0ebaf822bfcfd03e99cfa87c75cd6e
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-08-10 09:14:43 +0200

    Check whether file exists before sourcing it

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-source $HOME/scripts/semantic_update_service.properties
-source $HOME/scripts/start-service.properties
+SEMANTIC_PROPERTIES_FILE=$(dirname $BASH_SOURCE)/semantic_update_service.properties
+if [ -f "$SEMANTIC_PROPERTIES_FILE" ]; then
+    . $SEMANTIC_PROPERTIES_FILE
+fi
+START_PROPERTIES_FILE=$(dirname $BASH_SOURCE)/start-service.properties
+if [ -f "$START_PROPERTIES_FILE" ]; then
+    . $START_PROPERTIES_FILE
+fi
 jar=$ARTIFACT_ID
 jvmargs=$JVM_ARGS
 nohup java $jvmargs -jar "$jar.jar" &
