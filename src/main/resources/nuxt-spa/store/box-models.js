6b904d2f6dd506374fd15f14bf76bccd73051419
black <marius.appa@gmail.com>
2021-08-29 16:15:43 +0200

    start of box-model store

---
+++ src/main/resources/nuxt-spa/store/box-models.js
@@ -0,0 +1,26 @@
+export const state = () => ({
+  box_models: {
+    service_container: {
+      border:{
+        width:1,
+        unit:'px'
+      },
+      margin:{
+        width:0.35,
+        unit:'rem'
+      }
+    },
+    node_container:{
+      padding:{
+        width:0.5,
+        unit:'rem',
+      },
+      gap:{
+        width:0.6,
+        unit:'rem'
+      }
+    }
+  }
+});
+
+
