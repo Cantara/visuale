790e3bcf612525a0d2a4ec448cce2f4f682a7842
totto <totto@totto.org>
2022-01-03 16:27:12 +0100

    NPE

--- src/main/java/no/cantara/tools/visuale/status/StatusService.java
+++ src/main/java/no/cantara/tools/visuale/status/StatusService.java
@@ -227,7 +227,8 @@ public class StatusService implements Runnable {
                         for (Node node : nodesWithMatchingName) {
                             if (hasValue(health.getIp()) && hasValue(node.getIp()) && health.getIp().equalsIgnoreCase(node.getIp())) {
                                 // node matches name and ip
-                                if (health != null && node != null && !node.getVersion().equalsIgnoreCase(health.getVersion())) {
+                                if (health != null && node != null && node.getVersion() != null && health.getVersion() != null
+                                        && !node.getVersion().equalsIgnoreCase(health.getVersion())) {
                                     node.setVersion(health.getVersion());
                                 }
                                 node.addHealth(health);
