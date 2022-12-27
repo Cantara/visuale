36d6dc5e7907a462768c770be202f1270f1f230b
mbartl <mbartl@zoho.com>
2022-12-27 15:30:39 +0100

    implemented better date representation and better human-readable uptimes

--- src/main/resources/nuxt-spa/components/service/Node/NodeDetailedInfo.vue
+++ src/main/resources/nuxt-spa/components/service/Node/NodeDetailedInfo.vue
@@ -5,28 +5,33 @@
 
 <script>
     export default {
-        name: "JsonModal",
+      name: "JsonModal",
       props:{
           text: Object
       },
       computed:{
           formattedJson(){
-            //removes artifically created key to keep data raw again.
+            //removes artificially created key to keep data raw again.
             let formattedText = {...this.text};
             delete formattedText['key'];
             let json = JSON.stringify(formattedText,undefined,4);
             json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-            return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-              var cls = 'number';
+            return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)|(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})/g, function (match) {
+              let cls = 'number';
               if (/^"/.test(match)) {
-                if (/:$/.test(match)) {
+                if (/^"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})/.test(match)) {
+                  match = match.slice(1, -1);
+                  let date = new Date(Date.parse(match));
+                  match = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+                  cls = 'date';
+                } else if (/:$/.test(match)) {
                   cls = 'key';
                 } else {
                   cls = 'string';
                 }
               } else if (/true|false/.test(match)) {
                 cls = 'boolean';
-              } else if (/null/.test(match)) {
+              }  else if (/null/.test(match)) {
                 cls = 'null';
               }
               return '<span class="' + cls + '">' + match + '</span>';
@@ -39,11 +44,15 @@
 <style lang="scss">
 
 pre{
+
   text-align: left;
-  overflow-x: scroll;
-  overflow-y: scroll;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  overflow-wrap: break-word;
   max-height: 70vh;
-  padding:10px;
+  padding: 10px;
+  white-space: pre-wrap;       /* css-3 */
+  word-wrap: break-word;       /* Internet Explorer 5.5+ */
   color: $color--description;
   .boolean {
     color: $color--json-boolean;
@@ -53,6 +62,9 @@ pre{
   }
   .number {
     color: $color--json-number;
+  }
+  .date {
+    color: $color--json-date;
   }
   .key {
     color:$color--description-lighter;
--- src/main/resources/nuxt-spa/components/service/Node/NodeUptime.vue
+++ src/main/resources/nuxt-spa/components/service/Node/NodeUptime.vue
@@ -1,89 +1,98 @@
 <template>
-  <span>{{getRunningSince}}
-    <font-awesome-icon v-if="isGasPumpActive" class="warning"  :icon="faGasPump"/>
+  <span>{{ getRunningSince }}
+    <font-awesome-icon v-if="isGasPumpActive" class="warning" :icon="faGasPump"/>
   </span>
 </template>
 
 <script>
-  import { faGasPump,faPowerOff } from '@fortawesome/free-solid-svg-icons'
-  import {isNullOrUndefined} from "../../../utils/typeUtils";
+import {faGasPump, faPowerOff} from '@fortawesome/free-solid-svg-icons'
+import {isNullOrUndefined} from "../../../utils/typeUtils";
 
-  export default {
-    name: "NodeUptime",
-    props: {
-      health: {
-        required: true,
-        type: Object | undefined
-      },
-      shorten: {
-        required:false,
-        type:Boolean,
-        default:false
-      }
+export default {
+  name: "NodeUptime",
+  props: {
+    health: {
+      required: true,
+      type: Object | undefined
     },
-    data() {
-      return {
-        runningSince: null,
-        timer: null,
-        minutesSince :0
-      }
-    },
-    computed: {
-      faGasPump(){
-        return faGasPump;
-      },
-      getRunningSince() {
-        if(isNullOrUndefined(this.health))
-          return 'missing';
-        if(!this.health.hasOwnProperty('running since'))
-          return 'missing';
-        if(this.minutesSince < 60)
-          //small m since  it means minutes, Uppercase M means Months
-          return this.minutesSince +  (this.shorten ? 'm' :' Minutes');
-        if(this.minutesSince < 1440)
-          return Math.floor(this.minutesSince / 60) + (this.shorten ? 'H' : ' Hours');
-        if(this.minutesSince < 144000)
-          return Math.floor(this.minutesSince / 60 / 24) + (this.shorten ? 'D' : ' Days');
-        if(this.minutesSince >= 144000)
-          return Math.floor(this.minutesSince / 60 / 24 / 7) + (this.shorten ? 'W' : ' Weeks');
-      },
-      isGasPumpActive(){
-        return (this.minutesSince > 10080)
-      },
-    },
-    methods: {
-      startTimer () {
-        this.minutesSince = this.calculateMinuteSince(this.runningSince);
-        this.timer = setInterval(() => {
-         this.minutesSince = this.calculateMinuteSince(this.runningSince)
-        }, 60000)
-      },
-      calculateMinuteSince(runningSince) {
-        let d1 = new Date();
-        let d2 = new Date(runningSince);
-        return Math.floor((d1 - d2) / (1000 * 60));
-      }
-    },
-    mounted: function () {
-      this.runningSince = this.health['running since'];
-      this.startTimer();
-    },
-    watch: {
-      health: {
-        handler() {
-          this.runningSince = this.health['running since'];
-        },
-        deep: true
-      }
-    },
-    beforeDestroy () {
-      clearInterval(this.timer)
+    shorten: {
+      required: false,
+      type: Boolean,
+      default: false
     }
-
+  },
+  data() {
+    return {
+      runningSince: null,
+      timer: null,
+      minutesSince: 0
+    }
+  },
+  computed: {
+    faGasPump() {
+      return faGasPump;
+    },
+    getRunningSince() {
+      if (isNullOrUndefined(this.health))
+        return 'missing';
+      if (!this.health.hasOwnProperty('running since'))
+        return 'missing';
+      if (this.minutesSince < 60)
+        // small m since it means minutes, Uppercase M means Months
+        return this.minutesSince + (this.shorten ? 'm' : ' Minutes');
+      if (this.minutesSince < 1440)
+        return Math.floor(this.minutesSince / 60) + (this.shorten ? 'H' : ' Hours');
+      // less than 30 days -> show in days
+      if (this.minutesSince < 1440 * 30)
+        return Math.floor(this.minutesSince / 60 / 24) + (this.shorten ? 'D' : ' Days');
+      // less than 100 days -> show in weeks
+      if (this.minutesSince < 1440 * 100)
+        return Math.floor(this.minutesSince / 60 / 24 / 7) + (this.shorten ? 'W' : ' Weeks');
+      // more or equal 100 days -> show in months
+      // TODO for enhanced fun calculate real months by subtracting from "now"
+      if (this.minutesSince < 1440 * 365)
+        return Math.floor(this.minutesSince / 60 / 24 / 30) + (this.shorten ? 'M' : ' Months');
+      // more or equal 365 days -> show in years
+      if (this.minutesSince >= 1440 * 365)
+        return (this.minutesSince / 60 / 24 / 365).toFixed(1) + (this.shorten ? 'Y' : ' Years');
+    },
+    isGasPumpActive() {
+      return (this.minutesSince > 10080)
+    },
+  },
+  methods: {
+    startTimer() {
+      this.minutesSince = this.calculateMinuteSince(this.runningSince);
+      this.timer = setInterval(() => {
+        this.minutesSince = this.calculateMinuteSince(this.runningSince)
+      }, 60000)
+    },
+    calculateMinuteSince(runningSince) {
+      let d1 = new Date();
+      let d2 = new Date(runningSince);
+      return Math.floor((d1 - d2) / (1000 * 60));
+    }
+  },
+  mounted: function () {
+    this.runningSince = this.health['running since'];
+    this.startTimer();
+  },
+  watch: {
+    health: {
+      handler() {
+        this.runningSince = this.health['running since'];
+      },
+      deep: true
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.timer)
   }
+
+}
 </script>
 <style scoped>
-  span {
-    color:white;
-  }
+span {
+  color: white;
+}
 </style>
