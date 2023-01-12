1257c9c6cf99cb74e67907a5ce5d886eac073002
Daniel Berg <mail@roosta.sh>
2023-01-12 11:27:16 +0100

    Remove unused postcss plugins
    Fixes a bunch of build warnings, and it didn't seem like postcss did
    anything at all

--- src/main/resources/nuxt-spa/nuxt.config.js
+++ src/main/resources/nuxt-spa/nuxt.config.js
@@ -81,19 +81,12 @@ export default {
 
     ],
     postcss: {
-      // Add plugin names as key and arguments as value
-      // Install them before as dependencies with npm or yarn
-      plugins: {
-        // Disable a plugin by passing false as value
-        'postcss-url': false,
-        'postcss-nested': {},
-        'postcss-responsive-type': {},
-        'postcss-hexrgba': {}
-      },
       preset: {
-        // Change the postcss-preset-env settings
         autoprefixer: {
           grid: true
+        },
+        features: {
+          customProperties: false
         }
       }
     },
