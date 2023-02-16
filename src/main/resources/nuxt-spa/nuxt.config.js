5378da6e27c05fff0f693aa775ac490f0f0a1bc5
Daniel Berg <mail@roosta.sh>
2023-02-16 13:21:54 +0100

    Remove node-sass, rm unneded dep, fix postcss opts
    - node-sass is deprecated, replace with dart sass, if no one did this it
      would cause a lot of issue down the road. Most notably with webpack
      versions
    - Fix postcss option customProperties, supposed to be quoted
    - Remove vue-svg-loader, seems it isn't needed, the svg files can be
      imported without this no trouble

--- src/main/resources/nuxt-spa/nuxt.config.js
+++ src/main/resources/nuxt-spa/nuxt.config.js
@@ -86,7 +86,7 @@ export default {
           grid: true
         },
         features: {
-          customProperties: false
+          "custom-properties": false
         }
       }
     },
@@ -94,17 +94,6 @@ export default {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
-      const svgRule = config.module.rules.find(rule => rule.test.test('.svg'));
-
-      svgRule.test = /\.(png|jpe?g|gif|webp)$/;
-
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: [
-          'babel-loader',
-          'vue-svg-loader',
-        ],
-      });
     }
   }
 }
